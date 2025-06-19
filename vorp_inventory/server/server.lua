8f60004ffe27c35fbfcbdb9432d389ceebbcfad2
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-06-19 15:32:00 +0300

    Update server.lua

--- vorp_inventory/server/server.lua
+++ vorp_inventory/server/server.lua
@@ -1,35 +1,9 @@
-local Core <const>       = exports.vorp_core:GetCore()
-local InventoryBeingUsed = {}
-local T <const>          = TranslationInv.Langs[Lang]
-
-if Config.DevMode then
-    Log.Warning("^1[DEV] ^7You are in dev mode, dont use this in production live servers")
-end
+local Core <const> = exports.vorp_core:GetCore()
 
 RegisterServerEvent("syn:stopscene")
 AddEventHandler("syn:stopscene", function(x)
     local _source <const> = source
     TriggerClientEvent("inv:dropstatus", _source, x)
-end)
-
-RegisterServerEvent("vorpinventory:netduplog", function()
-    local _source <const> = source
-    local playername <const> = GetPlayerName(_source)
-    local description <const> = Logs.NetDupWebHook.Language.descriptionstart .. playername .. Logs.NetDupWebHook.Language.descriptionend
-
-    if Logs.NetDupWebHook.Active then
-        local info <const> = {
-            source = _source,
-            title = Config.NetDupWebHook.Language.title,
-            name = playername,
-            description = description,
-            webhook = Logs.NetDupWebHook.webhook,
-            color = Logs.NetDupWebHook.color
-        }
-        SvUtils.SendDiscordWebhook(info)
-    else
-        print('[' .. Logs.NetDupWebHook.Language.title .. '] ', description)
-    end
 end)
 
 RegisterServerEvent("vorp_inventory:Server:UnlockCustomInv", function()
@@ -38,36 +12,6 @@ RegisterServerEvent("vorp_inventory:Server:UnlockCustomInv", function()
         if value == _source then
             InventoryBeingUsed[i] = nil
             break
-        end
-    end
-end)
-
-AddEventHandler('playerDropped', function()
-    local _source <const> = source
-    if _source then
-        local user <const>    = Core.getUser(_source)
-        local weapons <const> = UsersWeapons.default
-
-        if AmmoData[_source] then
-            AmmoData[_source] = nil
-        end
-
-        for i, value in pairs(InventoryBeingUsed) do
-            if value == _source then
-                InventoryBeingUsed[i] = nil
-                break
-            end
-        end
-
-        if not user then return end
-
-        local charid <const> = user.getUsedCharacter.charIdentifier
-
-        for key, value in pairs(weapons) do
-            if value.charId == charid then
-                UsersWeapons.default[key] = nil
-                break
-            end
         end
     end
 end)
@@ -96,6 +40,6 @@ Core.Callback.Register("vorp_inventory:Server:CanOpenCustom", function(source, c
         return cb(true)
     end
 
-    Core.NotifyObjective(source, T.SomeoneUseing, 5000)
+    --Core.NotifyObjective(source, T.SomeoneUseing, 5000)
     return cb(false)
 end)
