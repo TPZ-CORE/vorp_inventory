d8bb4f3844c5793a4f592e58be8df15658779592
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-06-19 15:18:48 +0300

    Add files via upload

---
+++ vorp_inventory/fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+game 'rdr3'
+author 'VORP'
+name 'vorp inventory'
+description 'Inventory System for RedM VORPCore framework'
+
+lua54 'yes'
+
+client_scripts {
+  'client/exports.lua',
+  'client/client.lua',
+  'client/services/*.lua',
+  'client/controllers/*.lua',
+}
+
+server_scripts {
+  '@oxmysql/lib/MySQL.lua',
+  'server/vorpInventoryApi.lua',
+  'server/server.lua',
+  'server/services/*.lua',
+  'server/controllers/*.lua',
+}
+
+
+---@deprecated
+server_exports { 'vorp_inventoryApi' }
