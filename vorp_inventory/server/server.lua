2d14cac3b36d7f3176d8fd80f4e4fddaafc82364
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-06-19 15:40:02 +0300

    Update server.lua

--- vorp_inventory/server/server.lua
+++ vorp_inventory/server/server.lua
@@ -1,28 +1,9 @@
-local Core <const> = exports.vorp_core:GetCore()
-
-RegisterServerEvent("syn:stopscene")
-AddEventHandler("syn:stopscene", function(x)
-    local _source <const> = source
-    TriggerClientEvent("inv:dropstatus", _source, x)
-end)
-
 RegisterServerEvent("vorp_inventory:Server:UnlockCustomInv", function()
-    local _source <const> = source
-    for i, value in pairs(InventoryBeingUsed) do
-        if value == _source then
-            InventoryBeingUsed[i] = nil
-            break
-        end
-    end
+    -- nothing
 end)
 
 Core.Callback.Register("vorp_inventory:Server:CanOpenCustom", function(source, cb, id)
-    id = tostring(id)
-    if not InventoryBeingUsed[id] then
-        InventoryBeingUsed[id] = source
-        return cb(true)
-    end
-
-    --Core.NotifyObjective(source, T.SomeoneUseing, 5000)
-    return cb(false)
+    local container = exports.tpz_inventory:getInventoryAPI().getContainerData(id)
+    local status = container and (not container.busy) or true
+    return cb(status)
 end)
